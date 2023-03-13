533eae9effacfa2525deaf8489916cb2ba453334
jorcartriyo <jorcartriyo@gmail.com>
2023-03-13 16:49:41 +0100

    2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentacionesaportadas</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentacionesaportadas1</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
 Esta liena está cambiada                                </p>

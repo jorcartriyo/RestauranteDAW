0ce7a8000e208c800d1068147a966df1af3a999e
jorcartriyo <jorcartriyo@gmail.com>
2023-05-08 20:04:27 +0200

    portatil0805

--- resources/views/articulos/register.blade.php
+++ resources/views/articulos/register.blade.php
@@ -1,161 +1,160 @@
 @extends('layouts.appDashboard')
 @section('title', 'Artículos')
 @section('content')
-    <div class="container-fluid">
-        <div class="col-md-28 col-15 mr-auto ml-auto">
-            <form method="POST" id="myform" enctype="multipart/form-data" action="{{ route('home.store') }}">
-                @method('POST')
-                @csrf
-                <!--      Wizard container        -->
-                <div class="card card-wizard active" data-color="rose" id="wizardProfile">
-                    <!--        You can switch " data-color="primary" "  with one of the next bright colors: "green", "orange", "red", "blue"       -->
-                    <div class="card-header text-center">
-                        <h2 class="card-title">
-                            Crea un Artículo
-                        </h2>
-                    </div>
-                    {{--    Imagen Avatar --}}
-                    <div class="card-body">
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="about">
-                                <div class="row justify-content-center">
-                                    <div class="col-sm-4">
-                                        <div class="picture-container">
-                                            <div class="picture">
-                                                <img src="{{ asset('assets/img/default-avatar.png') }}" name="imgUsu"
-                                                    id="imgUsu" class="picture-src" title="avatar" />
-                                                <input type="file" id="file" name="file"
-                                                    onchange="mostrarImagen(event)">
-                                            </div>
-                                            <h6 class="description">Asigna la imagne</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                                {{--        Usuario --}}
 
-
-                                <div class="col-lg-18">
-                                    <div class="input-group form-control-lg">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">
-                                                <i class="material-icons">face</i>
-                                            </span>
+<div class="container-fluid">
+    <div class="col-md-28 col-15 mr-auto ml-auto">
+        <form method="POST" id="myform" enctype="multipart/form-data" action="{{ route('home.store') }}">
+            @method('POST')
+            @csrf
+            <!--      Wizard container        -->
+            <div class="card card-wizard active" data-color="rose" id="wizardProfile">
+                <!--        You can switch " data-color="primary" "  with one of the next bright colors: "green", "orange", "red", "blue"       -->
+                <div class="card-header text-center">
+                    <h2 class="card-title">
+                        Crea un Artículo
+                    </h2>
+                </div>
+                {{-- Imagen  --}}
+                <div class="card-body">
+             
+                        <div class="tab-pane active" id="about">
+                            <div class="row justify-content-center">
+                                <div class="col-sm-4">
+                                    <div class="picture-container">
+                                        <div class="picture">
+                                            <img src="{{ asset('images/products.png') }}" name="imgUsu" id="imgUsu" class="picture-src" title="avatar" />
+                                            <input type="file" id="file" name="file" onchange="mostrarImagen(event)">
                                         </div>
-                                        <div class="form-group">
-                                            <label for="name" class="bmd-label-floating">Nombre del Artículo
-                                                (required)</label>
-                                            <input type="text" class="form-control" id="name" name="name"
-                                                value='' required>
-                                        </div>
+                                        <h6 class="description">Asigna la imagen</h6>
                                     </div>
                                 </div>
-                            </div>
-                            {{--  email --}}
-                            <div class="col-lg-18">
-                                <div class="input-group form-control-lg">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">email</i>
-                                        </span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="email" class="bmd-label-floating">Descripcion
-                                            (required)</label>
-                                        <input type="email" class="form-control" id="email" name="email"
-                                            value='' required autocomplete="on">
-                                    </div>
-
-                                </div>
-                            </div>
-                            {{-- Password --}}
-                            <div class="col-lg-18">
-                                <div class="input-group form-control-lg">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">key</i>
-                                        </span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password" class="bmd-label-floating"> Categoria
-                                            (required)</label>
-                                        <input type="password" class="form-control" id="password" required="true"
-                                            value='' minLength="8" name="password" autocomplete="on">
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- Confirmar Password --}}
-                            <div class="col-lg-18">
-                                <div class="input-group form-control-lg">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">key</i>
-                                        </span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="confirmar_password" class="bmd-label-floating"> Precio
-                                            (required)</label>
-                                        <input type="password" class="form-control" id="password_confirmation"
-                                            required="true" value='' name="password_confirmation" autocomplete="on">
-                                    </div>
-                                </div>
-                            </div>
-                            {{-- Confirmar Password --}}
-                            <div class="col-lg-18">
-                                <div class="input-group form-control-lg">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">
-                                            <i class="material-icons">key</i>
-                                        </span>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="confirmar_password" class="bmd-label-floating"> Tipo
-                                            (required)</label>
-                                        <input type="password" class="form-control" id="password_confirmation"
-                                            required="true" value='' name="password_confirmation"
-                                            autocomplete="on">
-                                    </div>
-                                </div>
-                            </div>
-
-                                         {{-- Confirmar Password --}}
-                                         <div class="col-lg-18">
-                                            <div class="input-group form-control-lg">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text">
-                                                        <i class="material-icons">key</i>
-                                                    </span>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="confirmar_password" class="bmd-label-floating"> Activo
-                                                        (required)</label>
-                                                    <input type="password" class="form-control" id="password_confirmation"
-                                                        required="true" value='' name="password_confirmation"
-                                                        autocomplete="on">
-                                                </div>
-                                            </div>
-                                        </div>
-                            
-                            {{--  Roles --}}
-                            <div class="col-lg-18">
-                                <div class="text-center">
-                                    <h3>Asigna los Roles</h3>
-                                </div>
-                                <select class="duallistbox" multiple="multiple" name="rol[]">
-                             </select>
-                                <span id="mensaje"></span>
                             </div>
                         </div>
-                        {{-- Submit --}}
-                        <div>
-                            <div class="card-footer justify-content-around">
-                                <div class="ml-auto">
-                                    <button type="submit" class="btn btn-primary pull-right">Crear Art</button>
-                                </div>
-                            </div>
+                
+                </div>
+
+                {{-- Artículo --}}
+
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <img src="{{ asset('images/product.png') }}" alt="Articulo">
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="articulo" class="bmd-label-floating">Nombre del Artículo
+                                (requerido)</label>
+                            <input type="text" class="form-control" id="articulo" name="articulo" value='' required>
                         </div>
                     </div>
                 </div>
-            </form>
-        </div>
+
+                {{-- descripción --}}
+
+
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="material-icons">description</i>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="descripcion">Descripción del artículo</label>
+                            <textarea class="form-control rounded-0" style="resize: both;" id="descripcion" rows="10"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Categoria --}}
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <img class="material-icons" src="{{ asset('images/categoria.png') }}" alt="Categoria">
+                            </span>
+
+                        </div>
+                        <label for="categoria" class="bmd-label-floating"> Categoria
+                            (requerido)</label>
+                        <div class="form-group ml-5">
+
+                            <select id="categoria">
+                                <option value="Iron Man">Primeros</option>
+                                <option value=" Man">Segundos</option>
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                {{-- Precio --}}
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="material-icons">euro</i>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="precio" class="bmd-label-floating"> Precio
+                                (required)</label>
+                            <input type="number" step="0.01" class="form-control" id="precio" required="true" value='' name="precio" autocomplete="on">
+                        </div>
+                    </div>
+                </div>
+                {{-- Tipo--}}
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="material-icons">restaurant_menu</i>
+                            </span>
+                        </div>
+                        <label for="tipo" class="bmd-label-floating"> Tipo
+                            (required)</label>
+                        <div class="form-group ml-5">
+                            <input type="checkbox" id="carta" name="carta" value="carta">
+                            <label for="carta" class="px-2"> Carta</label><br>
+                            <input type="checkbox" id="menu" name="menu" value="menu">
+                            <label for="menu" class="px-2 mt-2"> Menú</label><br>
+
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Activo--}}
+                <div class="col-lg-18">
+                    <div class="input-group form-control-lg">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="material-icons">fact_check</i>
+                            </span>
+                        </div>
+                        <label for="activo" class="bmd-label-floating"> Activo
+                            (required)</label>
+                        <div class="form-group ml-5">
+                            <input type="radio" id="si" name="activo" value=1>
+                            <label class="px-2" for="si">Si</label><br>
+                            <input type="radio" id="no" name="activo" value=0>
+                            <label class="px-2 mt-2" for="no">No</label><br>
+                        </div>
+                    </div>
+                </div>
+
+                {{-- Submit --}}
+                <div>
+                    <div class="card-footer justify-content-around">
+                        <div class="ml-auto">
+                            <button type="submit" class="btn btn-primary pull-right">Crear Artículo</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
+    </form>
+</div>
+
+
 @endsection
